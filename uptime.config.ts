abe67204353b3efbbff5f913fc13e3af2b584831
zkf6 <zkkev53@gmail.com>
2025-09-16 12:55:06 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -17,47 +17,16 @@ const workerConfig = {
   // passwordProtection: 'username:password',
   // Define all your monitors here
   monitors: [
-    // Example HTTP Monitor
+     // Example TCP Monitor
     {
-      // `id` should be unique, history will be kept if the `id` remains constant
-      id: 'am.809098.xyz',
-      // `name` is used at status page and callback message
-      name: '博客',
-      // `method` should be a valid HTTP Method
-      method: 'GET',
-      // `target` is a valid URL
-      target: 'https://am.809098.xyz',
-      // [OPTIONAL] `tooltip` is ONLY used at status page to show a tooltip
-      tooltip: 'This is a tooltip for this monitor',
-      // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
-      statusPageLink: 'https://am.809098.xyz',
-      // [OPTIONAL] `expectedCodes` is an array of acceptable HTTP response codes, if not specified, default to 2xx
-      // expectedCodes: [200],
-      // [OPTIONAL] `timeout` in millisecond, if not specified, default to 10000
-      timeout: 10000,
-      // [OPTIONAL] headers to be sent
-      // headers: {
-      //   'User-Agent': 'Uptimeflare',
-      //   Authorization: 'Bearer YOUR_TOKEN_HERE',
-      // },
-      // [OPTIONAL] body to be sent
-      // body: 'Hello, world!',
-      // [OPTIONAL] if specified, the response must contains the keyword to be considered as operational.
-      // responseKeyword: 'success',
-      // [OPTIONAL] if specified, the check will run in your specified region,
-      // refer to docs https://github.com/lyc8503/UptimeFlare/wiki/Geo-specific-checks-setup before setting this value
-      // checkLocationWorkerRoute: 'https://am.809098.xyz',
-    },
-    // Example TCP Monitor
-    {
-      id: '809098.xyz',
-      name: 'Blog',
+      id: 'mtv',
+      name: 'mtv',
       // `method` should be `TCP_PING` for tcp monitors
       method: 'GET',
       // `target` should be `host:port` for tcp monitors
-      target: 'https://809098.xyz',
+      target: 'https://harold999-mtv.hf.space',
       tooltip: 'My production server monitor',
-      statusPageLink: 'https://809098.xyz',
+      statusPageLink: 'https://harold999-mtv.hf.space',
       timeout: 10000,
     },
   ],
